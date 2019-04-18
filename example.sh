1d31ab679e9ff28007815f67a77e87c0bf12493f
zhaozj89 <zzhaoao@connect.ust.hk>
2019-04-18 22:57:21 +0800

    Update

--- example.sh
+++ example.sh
@@ -3,14 +3,14 @@
 label_portion=0.9
 option='16000oneliners'
 
-win_size=5
-cp_rank=10
-neighbor=10
+win_size=30
+cp_rank=20
+neighbor=20
 
 seed=666
 
 python load_data.py --option $option --label_portion $label_portion --seed $seed
 python doc2vec.py --option $option --win_size $win_size --cp_rank $cp_rank
 python label_propagation.py --option $option --neighbor $neighbor
-#python fabp.py --option $option --neighbor $neighbor
+# python fabp.py --option $option --neighbor $neighbor
 
